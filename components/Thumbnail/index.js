c4e43d2dbaeca651fc64c88097aceea4c726fa22
Tomas Padilla <padillatomasagustin@gmail.com>
2021-01-17 12:01:05 +0100

    Extenral STYles JSX CSS

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -1,27 +1,23 @@
+// Importing EXTERNAL JSX CSS:
+import ThumbnailStyles from '../Thumbnail/styles';
+
 const Thumbnail = ({ caption, imageUrl }) => {
   // Function to prevent NULL
   const imageReal = () => {
     if (imageUrl == null) {
-      return (
-        <img
-          src='https://i2.wp.com/www.spainonafork.com/wp-content/uploads/2019/04/popcornHOR-11.png?fit=750%2C750&ssl=1'
-          alt='NoImgFound'
-          style={{ width: 210, height: 295, objectFit: 'cover' }}
-          className='thumb__image'
-        ></img>
-      );
+      return 'https://i2.wp.com/www.spainonafork.com/wp-content/uploads/2019/04/popcornHOR-11.png?fit=750%2C750&ssl=1';
     } else {
-      return (
-        <img src={imageUrl.medium} alt={caption} className='thumb__image'></img>
-      );
+      return imageUrl.medium;
     }
   };
 
   //   ::::: MAIN ::::::
   return (
     <div className='thumbnail'>
-      {imageReal()}
+      <img src={imageReal()} alt={caption} className='thumb__image' />
       <h3 className='thumb__caption'>{caption}</h3>
+      {/* Styles */}
+      <style jsx>{ThumbnailStyles}</style>
     </div>
   );
 };
---
+++ components/Thumbnail/styles.js
@@ -0,0 +1,14 @@
+import css from 'styled-jsx/css';
+
+const ThumbnailStyles = css`
+  .thumb__image {
+    width: 100%;
+    object-fit: cover;
+  }
+  .thumb__caption {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
+
+export default ThumbnailStyles;
